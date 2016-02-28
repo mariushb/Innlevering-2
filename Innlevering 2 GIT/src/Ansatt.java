ca7ec0a29ec920480da9c1631661db87f4569169
Marius <mariushb@gmail.com>
2016-02-28 01:20:21 +0100

    Oppretting av ansatt og gjest

---
+++ Innlevering 2 GIT/src/Ansatt.java
@@ -0,0 +1,13 @@
+import java.util.GregorianCalendar;
+
+public class Ansatt {
+
+private GregorianCalendar dateToday;
+
+	private int openKontortid = 7;
+
+	private int stengtKontortid = 17;
+
+public Ansatt(String fornavn, String etternavn, int pin)
+super(fornavn, etternavn, pin);
+}

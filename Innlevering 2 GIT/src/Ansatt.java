1461af18ac4d51033b30a641ddbdcb33c1601b94
Marius <mariushb@gmail.com>
2016-02-28 01:34:31 +0100

    Utførelse av v.03. Endring av ansatt, opprettelse av Konstanter

--- Innlevering 2 GIT/src/Ansatt.java
+++ Innlevering 2 GIT/src/Ansatt.java
@@ -7,10 +7,107 @@ private GregorianCalendar dateToday;
 	private int openKontortid = 7;
 
 	private int stengtKontortid = 17;
+	
+	private int ansinitet;
 
-public Ansatt(String fornavn, String etternavn, int pin)
+	private double timelonn;
+
+public Ansatt(String fornavn, String etternavn, int pin, int ansinitet, double timelonn))
 super(fornavn, etternavn, pin);
+this.ansinitet = ansinitet;
+
+this.timelonn = timelonn;
 }
+
+@Override
+
+public void settFornavn(String fornavn) {
+
+	super.setFornavn(fornavn);
+
+}
+
+
+
+
+@Override
+
+public String hentFornavn() {
+
+	return super.getFornavn();
+
+}
+
+
+
+
+@Override
+
+public void settEtternavn(String etternavn) {
+
+	super.setEtternavn(etternavn);
+
+}
+
+
+
+
+@Override
+
+public String hentEtternavn() {
+
+	return super.getEtternavn();
+
+}
+
+
+
+
+@Override
+
+public void settFulltNavn(String fornavn, String etternavn) {
+
+	super.setFornavn(fornavn);
+
+	super.setEtternavn(etternavn);
+
+}
+
+
+
+
+@Override
+
+public String hentFulltNavn() {
+
+	return super.getFornavn() + " " + super.getEtternavn();
+
+}
+
+
+
+
+@Override
+
+public double beregnKreditt() {
+
+	return timelonn * KREDITT;
+
+}
+
+
+
+
+@Override
+
+public double beregnBonus() {
+
+	return BONUS * ansinitet;
+
+}
+
+
+
 
 @Override
 
@@ -33,5 +130,20 @@ public boolean sjekkPIN(int pin) {
 		return this.pin == pin;
 
 	}
+
+}
+
+
+
+
+@Override
+
+public String toString() {
+
+	return "Ansatt \nKontortid: " + openKontortid + " - " + stengtKontortid + "\nKreditt: " + beregnKreditt()
+
+			+ "\nBonus: " + beregnBonus() + "\n" + super.toString();
+
+}
 
 }
---
+++ Innlevering 2 GIT/src/Konstanter.java
@@ -0,0 +1,42 @@
+public interface Konstanter {
+
+	final int KREDITT = 2;
+
+	final int BONUS = 3000;
+
+
+
+
+	public void settFornavn(String fornavn);
+
+	
+
+	public String hentFornavn();
+
+	
+
+	public void settEtternavn(String etternavn);
+
+	
+
+	public String hentEtternavn();
+
+	
+
+	public void settFulltNavn(String fornavn, String etternavn);
+
+	
+
+	public String hentFulltNavn();
+
+	
+
+	public double beregnKreditt();
+
+	
+
+	public double beregnBonus();
+
+	
+
+}

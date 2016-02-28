953dcaa66d000f6d2b2930b1915170f1ce42ff72
Marius <mariushb@gmail.com>
2016-02-28 01:24:05 +0100

    Oprettelse av Ansatt og Gjest

--- Innlevering 2 GIT/src/Ansatt.java
+++ Innlevering 2 GIT/src/Ansatt.java
@@ -11,3 +11,27 @@ private GregorianCalendar dateToday;
 public Ansatt(String fornavn, String etternavn, int pin)
 super(fornavn, etternavn, pin);
 }
+
+@Override
+
+public boolean sjekkPIN(int pin) {
+
+	this.dateToday = new GregorianCalendar();
+
+	if (isSperret()) {
+
+		return false;
+
+	} else if ((dateToday.HOUR_OF_DAY >= openKontortid) && (dateToday.HOUR_OF_DAY <= stengtKontortid)
+
+			&& (dateToday.DAY_OF_WEEK > 1) && (dateToday.DAY_OF_WEEK < 7)) {
+
+		return true;
+
+	} else {
+
+		return this.pin == pin;
+
+	}
+
+}
--- Innlevering 2 GIT/src/Gjest.java
+++ Innlevering 2 GIT/src/Gjest.java
@@ -1,4 +1,52 @@
+import java.util.GregorianCalendar;
 
 public class Gjest {
 
+	private int dagerGyldig = 7;
+
+	private GregorianCalendar gyldigTilDato;
+
+	private GregorianCalendar dateToday;
+
+
+public Gjest(String fornavn, String etternavn) {
+
+		super(fornavn, etternavn, 9999);
+
+		this.gyldigTilDato = new GregorianCalendar();
+
+		gyldigTilDato.add(GregorianCalendar.DAY_OF_MONTH, dagerGyldig);
+
+	}
+
+
+
+
+	public GregorianCalendar getGyldigTilDato() {
+
+		return gyldigTilDato;
 }
+
+@Override
+
+	public boolean sjekkPIN(int pin) {
+
+		this.dateToday = new GregorianCalendar();
+
+		if (isSperret()) {
+
+			return false;
+
+		} else if (gyldigTilDato.before(dateToday)) {
+
+			super.setSperretKort(true);
+
+			return false;
+
+		} else {
+
+			return this.pin == pin;
+
+		}
+
+	}
